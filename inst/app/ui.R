3caff565614d7f7f9f76a47c7b10bb0a4e4c3d53
= <william.ray@hsc.utah.edu>
2025-02-04 15:42:04 -0700

    update ui

--- inst/app/ui.R
+++ inst/app/ui.R
@@ -10,33 +10,114 @@ library(bslib)
 library(bslib)
 
 
-ui <- fluidPage(
+ui <- bslib::page_fluid(gap = 0,
+  tags$head(tags$style(
+    "table {font-size: 10pt;
+    border-collapse: collapse;
+    compact; nowrap;}
+    th, tr, td {padding: 0px;
+            border-spacing: 0px;
+            margin: 0px;}"
+  )),
   theme = bslib::bs_theme(version = 5, bootswatch = "minty"),
+  h3("Application Title"),
+  layout_column_wrap(
+    width = "250px",
+    card(
+      card_header("Population A"),
+      tags$table(
+        tags$tr(
+          tags$td(p("Population Size")),
+          tags$td(numericInput(inputId = "popsize_a", value = 80000, label = "", width = "100px"))
+        ),
+        tags$tr(
+          tags$td(p("Fraction Vaccinated")),
+          tags$td(numericInput(inputId = "vac_portion_a", value = 0.1, label = "", width = "100px"))
 
-  titlePanel("Vaccine Equity"),
-
-
-
-
-  bslib::layout_column_wrap(width = 1 / 3,
-    card(bslib::layout_column_wrap(width = 1 / 2),
-      "Population A",
-
-      textInput(inputId = "vacPortion_a", label = "fraction vaccinated", value = "0.1", width = "10%")
+        ),
+        tags$tr(
+          tags$td("Contact Rate"),
+          tags$td(numericInput(inputId = "contact_rate_a", value = 100, label = "", width = "100px"))
+        )
+      ),
+      full_screen = TRUE
     ),
-    card("Ratios"),
-    card("Population B")
+    card(
+      card_header("Ratios"),
+      tags$table(
+        tags$tr(
+          tags$td("Vaccination time"),
+          tags$td(numericInput(inputId = "Vaccination_time", value = 100, label = "", width = "100px"))
+
+        ),
+        tags$tr(
+          tags$td("Recovry Rate"),
+          tags$td(numericInput(inputId = "recovery_rate", value = 0.1, label = "", width = "100px"))
+
+        ),
+        tags$tr(
+          tags$td("R0"),
+          tags$td(numericInput(inputId = "r0", value = 1.2, label = "", width = "100px"))
+
+        ),
+        tags$tr(
+          tags$td("Amount to spend"),
+          tags$td(numericInput(inputId = "amount_to_spend", value = 1e5, label = "", width = "100px"))
+
+        ),
+        tags$tr(
+          tags$td("Contact Ratio"),
+          tags$td(numericInput(inputId = "contact_ratio", value = 1.2, label = "", width = "100px"))
+
+        ),
+        tags$tr(
+          tags$td("Susceptibility Ratio"),
+          tags$td(numericInput(inputId = "susc_ratio", value = 1.2, label = "", width = "100px"))
+
+        ),
+        tags$tr(
+          tags$td("Vaccine cost ratio"),
+          tags$td(numericInput(inputId = "cost_ratio", value = 1.2, label = "", width = "100px"))
+
+        )
+
+      ),
+
+      full_screen = TRUE
+    ),
+    card(
+      card_header("Population B"),
+      tags$table(
+        tags$tr(
+          tags$td(p("Population Size")),
+          tags$td(numericInput(inputId = "popsize_a", value = 80000, label = "", width = "100px"))
+        ),
+        tags$tr(
+          tags$td(p("Fraction Vaccinated")),
+          tags$td(numericInput(inputId = "vac_portion_a", value = 0.1, label = "", width = "100px"))
+        ),
+
+        tags$tr(
+          tags$td("Contact Rate"),
+          tags$td(numericInput(inputId = "contact_rate_a", value = 100, label = "", width = "100px"))
+        )
+      ),
+      full_screen = TRUE
+    )
+  ),
+  absolutePanel(bottom = 0, left = 0, right = 0, fixed = TRUE,
+    img(src = "figs/logo.jpg", align = "right", height = 100, width = 100)
   )
-
-
-
-
-
-
-
-
-
 )
+
+
+
+
+
+
+
+
+
 
 
 
