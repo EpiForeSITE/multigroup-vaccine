247cfae04b13ea857b8465efff4795d4e211b8e8
= <william.ray@hsc.utah.edu>
2025-02-07 14:59:35 -0700

    added text on about tab

--- inst/app/server.R
+++ inst/app/server.R
@@ -1,6 +1,6 @@
 library(shiny)
 
-
+source("global.R")
 
 server <- function(input, output, session) {
 
--- inst/app/ui.R
+++ inst/app/ui.R
@@ -11,37 +11,55 @@ library(bslib)
 
 myToolTip <- function(tipText = NULL) {
   if (!is.null(tipText)) {
-    return(
-      tooltip(
-        img(src = "figs/tooltip.png", height = 20, width = 20),
-        tipText
-      )
-    )
+    return(tooltip(img(
+      src = "figs/tooltip.png",
+      height = 20,
+      width = 20
+    ), tipText))
   }
 }
 
-double_input_row <- function(input_ida, input_idb, label, valuea, valueb, tip = NULL) {
-  tags$tr(
+double_input_row <- function(input_ida,
+                             input_idb,
+                             label,
+                             valuea,
+                             valueb,
+                             tip = NULL) {
+  tags$tr(tags$td(myToolTip(tip), label),
     tags$td(
-      myToolTip(tip),
-      label
+      numericInput(
+        inputId = input_ida,
+        value = valuea,
+        label = NULL,
+        width = "100px"
+      )
     ),
-    tags$td(numericInput(inputId = input_ida, value = valuea, label = NULL, width = "100px")),
-    tags$td(numericInput(inputId = input_idb, value = valueb, label = NULL, width = "100px"))
-  )
+    tags$td(
+      numericInput(
+        inputId = input_idb,
+        value = valueb,
+        label = NULL,
+        width = "100px"
+      )
+  ))
 }
 
 single_input_row <- function(input_id, label, value, tip = NULL) {
-  tags$tr(
-    tags$td(myToolTip(tip),
-      label),
-    tags$td(numericInput(inputId = input_id, value = value, label = NULL, width = "100px"))
-  )
+  tags$tr(tags$td(myToolTip(tip), label), tags$td(
+    numericInput(
+      inputId = input_id,
+      value = value,
+      label = NULL,
+      width = "100px"
+    )
+  ))
 }
 
-ui <- page_fluid(gap = 0,
-  tags$head(tags$style(
-    "table {font-size: 8pt;
+ui <- page_fluid(
+  gap = 0,
+  tags$head(
+    tags$style(
+      "table {font-size: 8pt;
                        border-collapse: collapse;
                        compact; nowrap;}
                        th, tr, td {padding: 0px;
@@ -52,11 +70,13 @@ ui <- page_fluid(gap = 0,
                           padding-block: 0px;
                           padding-inline: 0px;
                        }"
-  )),
+    )
+  ),
   theme = bslib::bs_theme(version = 5, bootswatch = "pulse"),
   titlePanel("Vaccine Strategies in Disparate Populations"),
   navset_tab(
-    nav_panel("Model",
+    nav_panel(
+      "Model",
 
 
       layout_column_wrap(
@@ -64,25 +84,34 @@ ui <- page_fluid(gap = 0,
         card(
           card_title("Population Setup"),
           tags$table(
-            tags$tr(
-              tags$td(""),
-              tags$th("Population A"),
-              tags$th("Population B")
-
-            ),
+            tags$tr(tags$td(""), tags$th("Population A"), tags$th("Population B")),
 
             double_input_row("popsize_a", "popsize_b", "Population Size", 80000, 20000, tip = "Size of each population"),
-            double_input_row("vacPortion_a", "vacPortion_b", "Fraction Vaccinated", 0.1, 0.1, tip = "Fraction of each population that is vaccinated at time zero."),
-            double_input_row("contactWithinGroup_a", "contactWithinGroup_b", "Fraction of contacts within-group", 0.4, 0.4, tip = "This is the propotion of contacts made by individuals in each population that are within their own group."),
+            double_input_row(
+              "vacPortion_a",
+              "vacPortion_b",
+              "Fraction Vaccinated",
+              0.1,
+              0.1,
+              tip = "Fraction of each population that is vaccinated at time zero."
+            ),
+            double_input_row(
+              "contactWithinGroup_a",
+              "contactWithinGroup_b",
+              "Fraction of contacts within-group",
+              0.4,
+              0.4,
+              tip = "This is the propotion of contacts made by individuals in each population that are within their own group."
+            )
           )
         ),
         card(
           card_title("Parameters"),
           tags$table(
-            single_input_row("vacTime", "Vaccination time", 0, tip = "Time when vaccine will be started"),
+            single_input_row("vacTime", "Vaccination start time (days)", 0, tip = "Time when vaccine will be started"),
             single_input_row("recoveryRate", "Recovery Rate", 0.1, tip = "Probability per day of a infected individual recovering"),
-            single_input_row("R0", "R0", 1.2, tip = "duh"),
-            single_input_row("amountToSpend", "Amount to spend", 1e5, tip = "Funds available for vacination ($)"),
+            single_input_row("R0", "R0", 1.2, tip = "Basic Reprodoctive Number"),
+            single_input_row("amountToSpend", "Amount to spend", 1e5, tip = "Funds available for vacination ($)")
 
 
           )
@@ -90,20 +119,39 @@ ui <- page_fluid(gap = 0,
         card(
           card_title("Population Ratios"),
           tags$table(
-
-            single_input_row("contactRatio", "Contact Ratio", 1.1),
-            single_input_row("suscRatio", "Susceptibility Ratio", 1.2),
-            single_input_row("vaccineCostRatio", "Vaccine cost ratio", 1.3)
+            single_input_row("contactRatio", "Contact Ratio", 1.1, tip = "The ratio of the contact rate of the second group to the first group"),
+            single_input_row("suscRatio", "Susceptibility Ratio", 1.2, tip =
+              "The ratio of the susceptibility of the second group to the first group"),
+            single_input_row("vaccineCostRatio", "Vaccine cost ratio", 1.3, tip =
+              "The ratio of the cost of the vaccine for the second group to the first group")
 
           )
         )
       ),
       plotOutput("plot", click = "plot_click", ),
     ),
-    nav_panel("About")),
+    nav_panel(
+      "About",
+      h3("Abstract:"),
+      p(
+        "Addressing disparities in vaccine uptake through equity-targeted outreach programs requires additional funding, but the cost and outcome trade-offs are not well-understood. This study compared the overall and distributional health and cost outcomes of different vaccination programs."
+      ),
+      tags$i("D. Nguyen1 ∙ K. Duong2 ∙ E. Coates3 ∙ R.E. Nelson4 ∙ J. Love4 ∙ M.M. Jones4 ∙ M. Samore5 ∙ N. Chaiyakunapruk6 ∙ D. Toth"),
+      a("https://doi.org/10.1016/j.jval.2024.03.039", href = "https://doi.org/10.1016/j.jval.2024.03.039", class = "link-class", id = "paper_link")
+    )
+  ),
 
 
-  absolutePanel(bottom = 0, left = 0, right = 0, fixed = TRUE,
-    img(src = "figs/logo.jpg", align = "right", height = 100, width = 100)
+  absolutePanel(
+    bottom = 0,
+    left = 0,
+    right = 0,
+    fixed = TRUE,
+    img(
+      src = "figs/logo.jpg",
+      align = "right",
+      height = 100,
+      width = 100
+    )
   )
 )
