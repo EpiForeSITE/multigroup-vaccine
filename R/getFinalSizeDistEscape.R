98c7b6eb0affc601a856bdec6e1354f8d894d40b
damontoth <Damon.Toth@hsc.utah.edu>
2025-07-07 14:33:57 -0600

    Added age-structured model vignette and associated functions used

---
+++ R/getFinalSizeDistEscape.R
@@ -0,0 +1,40 @@
+getFinalSizeDistEscape <- function(n, transmrates, recoveryrate, popsize, initR, initI, initV) {
+  fsODE <- round(getFinalSizeODE(transmrates, recoveryrate, popsize, initR, initI, initV)$totalSize)
+
+  g <- length(popsize) # number of groups
+  e <- g * 2           # number of distinct events
+  betaoverNj <- t(t(transmrates) / popsize)
+  initS <- popsize - initR - initV
+  Rtally <- matrix(0, n, g)
+
+  for (r in 1:n) {
+    I <- initI
+    S <- initS
+    R <- initR
+
+    while (sum(I) > 0) {
+      tr <- rowSums(outer(S, I) * betaoverNj)
+      rr <- I * recoveryrate
+
+      logprobend <- sum(I) * log(recoveryrate / sum(tr))
+
+      if(logprobend < -40){
+        R <- fsODE
+        I <- rep(0, g)
+      }else{
+
+        event <- sample(e, 1, prob = c(tr, rr))
+
+        if (event > g) {
+          I[event - g] <- I[event - g] - 1
+          R[event - g] <- R[event - g] + 1
+        } else {
+          S[event] <- S[event] - 1
+          I[event] <- I[event] + 1
+        }
+      }
+    }
+    Rtally[r, ] <- R
+  }
+  Rtally
+}
---
+++ R/getOutputTable.R
@@ -0,0 +1,88 @@
+getOutputTable <- function(agelims, agepops, agecovr, ageveff, initgrp){
+
+  grpnames <- c(paste0("under", agelims[2]),
+                paste0(agelims[2:(length(agelims)-1)], "to", agelims[3:length(agelims)]-1),
+                paste0(agelims[length(agelims)], "+"))
+
+  agevacimmune <- round(agepops * agecovr * ageveff)
+
+  pops <- agepops
+  initV <- agevacimmune
+
+  initI <- rep(0, length(pops))
+  initI[initgrp] <- 1
+  initR <- rep(0, length(pops))
+
+  R0vals <- 10:18
+  meaninf <- 7
+
+  data(polymod)
+  suppressWarnings (
+  cm <- contact_matrix(polymod,
+                       age.limits = agelims,
+                       symmetric = TRUE,
+                       missing.participant.age = "remove",
+                       missing.contact.age = "remove")
+  )
+
+  nj <- cm$demography$proportion
+  mij <- t(t(cm$matrix) / nj * (pops / sum(pops)))
+
+  relcontact <- rowSums(mij)
+  cmat <- mij / relcontact
+
+  Rv <- rep(0, length(R0vals))
+  escapesize <- matrix(0, length(R0vals), length(pops))
+
+  for(i in seq_along(R0vals)) {
+    betaij <- transmissionRates(
+      R0 = R0vals[i],
+      meaninf = meaninf,
+      reltransm = mij
+    )
+    Rv[i] <- repnum(meaninf, agepops, betaij, initR, agecovr * agepops, ageveff)
+
+    if(Rv[i] < 1){
+      escapesize[i, ] <- rep(0, length(pops))
+    }else{
+      escapesize[i, ] <- getFinalSizeODE(
+        transmrates = betaij,
+        recoveryrate = 1 / meaninf,
+        popsize = pops,
+        initR = initR,
+        initI = initI,
+        initV = initV
+      )$totalSize
+    }
+    escapesizetot <- rowSums(escapesize)
+  }
+
+  numsims <- 10000
+
+  probescape <- rep(0, length(R0vals))
+  for(i in seq_along(R0vals)) {
+    if(Rv[i] < 1) {
+      probescape[i] <- 0
+    } else {
+      betaij <- transmissionRates(
+        R0 = R0vals[i],
+        meaninf = meaninf,
+        reltransm = mij
+      )
+      size_dist <- getFinalSizeDistEscape(
+        n = numsims,
+        transmrates = betaij,
+        recoveryrate = 1 / meaninf,
+        popsize = pops,
+        initR = initR,
+        initV = initV,
+        initI = initI
+      )
+      probescape[i] <- sum(rowSums(size_dist) > escapesizetot[i] * 0.9) / numsims
+    }
+  }
+
+  tbl <- cbind(R0vals, Rv, probescape, round(escapesizetot), round(escapesize))
+  colnames(tbl) <- c("R0", "Rv", "pEscape", "escapeInfTot", grpnames)
+  tbl
+}
