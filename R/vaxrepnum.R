85ef5c7f3137d20ab6a270d7339154bac04a2447
damontoth <Damon.Toth@hsc.utah.edu>
2025-12-18 14:05:56 -0700

    add function documentation

--- R/vaxrepnum.R
+++ R/vaxrepnum.R
@@ -1,5 +1,11 @@
-# TODO: Add documentation and @keywords internal flag unless we want it to be exported
-#' @keywords internal
+#' Calculate reproduction number for a multigroup model with a given state of
+#' vaccination and immunity
+#' @param meaninf mean infectious period with same time units as trmat
+#' @param popsize the population size of each group
+#' @param initR initial number of each group already infected and immune
+#' @param initV initial number of each group vaccinated
+#' @param vaxeff effectiveness (0 to 1) of vaccine in producing immunity to infection
+#' @export
 vaxrepnum <- function(meaninf, popsize, trmat, initR, initV, vaxeff) {
 
   betaij <- trmat * (1 - (initR + initV * vaxeff) / popsize)
