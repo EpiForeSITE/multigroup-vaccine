f704722c0bc9a9408365bdcd2c5f8cbb04a1ff4b
damontoth <Damon.Toth@hsc.utah.edu>
2026-01-20 10:42:06 -0700

    add output documentation to contactMatrixPropPref()

--- R/contactMatrixPropPref.R
+++ R/contactMatrixPropPref.R
@@ -2,6 +2,8 @@
 #' @param popsize population size of each group
 #' @param contactrate overall contact rate of each group
 #' @param ingroup fraction of each group's contacts that are exclusively in-group
+#' @returns a square matrix with the contact rate of each group (row) with members of each
+#' other group (column)
 #' @examples
 #' contactMatrixPropPref(popsize = c(100, 150, 200), contactrate = c(1.1, 1, 0.9),
 #' ingroup = c(0.2, 0.25, 0.22))
