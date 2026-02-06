d39dd692963a9cfbfe09c3936c8662eaf21d631d
damontoth <Damon.Toth@hsc.utah.edu>
2026-02-06 10:31:29 -0700

    draft simStochasticOutbreak()

---
+++ R/simStochasticOutbreak.R
@@ -0,0 +1,56 @@
+simStochasticOutbreak <- function(n, transmrates, recoveryrate, popsize, initR, initI, initV) {
+  g <- length(popsize) # number of groups
+  e <- g * 2           # number of distinct events
+  betaoverNj <- t(t(transmrates) / popsize)
+  initS <- popsize - initR - initV
+  Rtally <- matrix(0, n, g)
+  ind <- 1
+  tsim <- rep(NA, n * 10)
+  for (r in 1:n) {
+    I <- initI
+    S <- initS
+    R <- initR
+
+    tm <- 0
+    tsim[ind] <- 0
+    ind <- ind + 1
+    if(ind > length(tsim)){
+      length(tsim) <- length(tsim) * 2
+    }
+    while (sum(I) > 0) {
+      tr <- rowSums(outer(S, I) * betaoverNj)
+      rr <- I * recoveryrate
+      rates <- c(tr, rr)
+
+      event <- sample(e, 1, prob = rates)
+      tm <- tm + rexp(1, sum(rates))
+
+      if (event > g) {
+        I[event - g] <- I[event - g] - 1
+        R[event - g] <- R[event - g] + 1
+      } else {
+        S[event] <- S[event] - 1
+        I[event] <- I[event] + 1
+
+        tsim[ind] <- tm
+        ind <- ind + 1
+        if(ind > length(tsim)){
+          length(tsim) <- length(tsim) * 2
+        }
+      }
+    }
+    Rtally[r, ] <- R
+  }
+  x <- rowSums(Rtally)
+  y <- tsim[!is.na(tsim)]
+
+  xc <- cumsum(c(1, x))
+  max_len <- max(x)
+  last_vals <- y[cumsum(x)]
+  mp <- matrix(last_vals, nrow = n, ncol = max_len, byrow = FALSE)
+  row_idx <- rep(1:n, times = x)
+  col_idx <- sequence(x)
+  mp[cbind(row_idx, col_idx)] <- y
+
+  list(Rtally, tsim[!is.na(tsim)], mp)
+}
