bd6c362717b6e6c2ee7403c6d522fe2e0522b7f8
damontoth <Damon.Toth@hsc.utah.edu>
2026-02-12 17:23:43 -0700

    modify function output

--- R/simStochasticOutbreak.R
+++ R/simStochasticOutbreak.R
@@ -3,20 +3,24 @@ simStochasticOutbreak <- function(n, transmrates, recoveryrate, popsize, initR, 
   e <- g * 2           # number of distinct events
   betaoverNj <- t(t(transmrates) / popsize)
   initS <- popsize - initR - initV
+  nI <- sum(initI)
   Rtally <- matrix(0, n, g)
   ind <- 1
-  tsim <- rep(NA, n * 10)
+  tsim <- gsim <- rep(NA, n * nI * 10)
+  gsimInit <- rep(which(initI > 0), initI[initI > 0])
   for (r in 1:n) {
     I <- initI
     S <- initS
     R <- initR
 
     tm <- 0
-    tsim[ind] <- 0
-    ind <- ind + 1
-    if(ind > length(tsim)){
-      length(tsim) <- length(tsim) * 2
+    if(ind + nI - 1 > length(tsim)){
+      length(tsim) <- length(gsim) <- length(tsim) * 2
     }
+    tsim[ind:(ind + nI - 1)] <- 0
+    gsim[ind:(ind + nI - 1)] <- gsimInit
+    ind <- ind + nI
+
     while (sum(I) > 0) {
       tr <- rowSums(outer(S, I) * betaoverNj)
       rr <- I * recoveryrate
@@ -33,24 +37,27 @@ simStochasticOutbreak <- function(n, transmrates, recoveryrate, popsize, initR, 
         I[event] <- I[event] + 1
 
         tsim[ind] <- tm
+        gsim[ind] <- event
         ind <- ind + 1
         if(ind > length(tsim)){
-          length(tsim) <- length(tsim) * 2
+          length(tsim) <- length(gsim) <- length(tsim) * 2
         }
       }
     }
     Rtally[r, ] <- R
   }
-  x <- rowSums(Rtally)
+
+  x <- rep(1:n, rowSums(Rtally))
   y <- tsim[!is.na(tsim)]
+  z <- gsim[!is.na(tsim)]
 
-  xc <- cumsum(c(1, x))
-  max_len <- max(x)
-  last_vals <- y[cumsum(x)]
-  mp <- matrix(last_vals, nrow = n, ncol = max_len, byrow = FALSE)
-  row_idx <- rep(1:n, times = x)
-  col_idx <- sequence(x)
-  mp[cbind(row_idx, col_idx)] <- y
-
-  list(Rtally, tsim[!is.na(tsim)], mp)
+  list(total = Rtally, inftime = cbind(sim = x, inftime = y, group = z))
+  #xc <- cumsum(c(1, x))
+  #max_len <- max(x)
+  #last_vals <- y[cumsum(x)]
+  #mp <- matrix(last_vals, nrow = n, ncol = max_len, byrow = FALSE)
+  #row_idx <- rep(1:n, times = x)
+  #col_idx <- sequence(x)
+  #mp[cbind(row_idx, col_idx)] <- y
+  #mp
 }
