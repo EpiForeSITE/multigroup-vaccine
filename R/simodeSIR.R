659430dfd0ce4bdaaac0d4b618cd6c5a4872caca
damontoth <Damon.Toth@hsc.utah.edu>
2026-03-02 16:33:01 -0700

    modify examples and add tests for simodeSIR()

--- R/simodeSIR.R
+++ R/simodeSIR.R
@@ -6,15 +6,17 @@
 #' @param initR initial number of each group already infected and removed (included in final size)
 #' @param initI initial number of each group infectious
 #' @param initV initial number of each group vaccinated
-#' @param times optional argument for times at which the solution is to be calculated; if NA (the
-#' default) the solution will be computed up to the time at which the total number of infectious
-#' individuals is sufficiently close to zero (outbreak extinction)
+#' @param times optional argument for times at which the solution is to be calculated; if NULL
+#' (the default) the solution will be computed up to the time at which the total number of
+#' infectious individuals is sufficiently close to zero (outbreak extinction)
 #' @returns vector of final sizes (number of infected over whole outbreak) for each group
 #' @examples
 #' simodeSIR(transmrates = matrix(0.2, 2 ,2), recoveryrate = 0.3,
 #' popsize = c(100, 150), initR = c(0, 0), initI = c(0, 1), initV = c(10, 10))
+#' simodeSIR(transmrates = matrix(0.2, 2 ,2), recoveryrate = 0.3,
+#' popsize = c(100, 150), initR = c(0, 0), initI = c(0, 1), initV = c(10, 10), times = 0:10)
 #' @export
-simodeSIR <- function(transmrates, recoveryrate, popsize, initR, initI, initV, times = NA) {
+simodeSIR <- function(transmrates, recoveryrate, popsize, initR, initI, initV, times = NULL) {
 
   betaoverNj <- t(t(transmrates) / popsize)
 
@@ -27,7 +29,7 @@ simodeSIR <- function(transmrates, recoveryrate, popsize, initR, initI, initV, t
                  paste0("R", 1:length(popsize)))
 
   parms <- c(betaoverNj, recoveryrate)
-  if(is.na(times)){
+  if(is.null(times)){
     tm <- seq(0, 10000 / recoveryrate, len = 10000)
     rootfun <- function(t, y, parms) sum(y[(length(popsize) + 1):(length(popsize) * 2)]) - sqrt(.Machine$double.eps)
     sim <- deSolve::ode(y0, tm, odeSIR, parms, rootfun = rootfun, method = "lsodar")
---
+++ tests/testthat/test-simodeSIR.R
@@ -0,0 +1,18 @@
+test_that("simodeSIR returns correct dimensions", {
+  times <- 0:30
+  transmrates <- matrix(0.2, 2, 2)
+  recoveryrate <- 0.3
+  popsize <- c(100, 150)
+  initR <- c(0, 0)
+  initI <- c(1, 0)
+  initV <- c(10, 10)
+
+  result <- simodeSIR(transmrates, recoveryrate, popsize, initR, initI, initV, times)
+
+  # Should return matrix of the correct dimensions
+  expect_equal(nrow(result), length(times))
+  expect_equal(ncol(result), length(popsize) * 3)
+
+  # Sum of all compartments should match the non-vaccinated population total
+  expect_all_equal(rowSums(result[, -1]), sum(popsize) - sum(initV))
+})
